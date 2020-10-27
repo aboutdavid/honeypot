1a96e44c1a6b71ec453c4128ba4af489f3bd1836
Glitch (hello-express) <none>
2020-10-27 20:39:08 +0000

    🏠🎺 Checkpoint ./server.js:8045597/1114 ./routes.js:8045597/11

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const routes = require("./routes.js");
 
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/:route", (req, res) => {
-  
+  if (routes[req.query.route]) {
+    res.send(routes[req.query.route][1])
+    console.log(routes[req.query.route][1])
+  } else {
+    res.send("Not found. Sorry :/");
+  }
 });
 
 const listener = app.listen(process.env.PORT, () => {
