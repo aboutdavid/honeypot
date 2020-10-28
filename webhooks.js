757c5422af957efa294f4177559ac8d1c1fdec76
Glitch (hello-express) <none>
2020-10-28 15:51:44 +0000

    🌴📫 Checkpoint ./webhooks.js:8045597/1632

--- webhooks.js
+++ webhooks.js
@@ -1,13 +1,20 @@
 const webhook = require("webhook-discord");
 
-function send(date, path, desc, ip){
-const Hook = new webhook.Webhook("https://discord.com/api/webhooks/771035146115481630/DY6_A9MS0lgNsBMrMkauynW9OaUYIwofSqVA4SSVB2Ly9IBlbeNlxyIc7-CmFrz-fpIs");
- 
-const msg = new webhook.MessageBuilder()
-                .setName("🍯 Honeypot")
-                .setColor("#00fa9a")
-                .setText("This is my webhook!");
-Hook.send(msg); 
+function send(date, path, desc, ip) {
+  const Hook = new webhook.Webhook(
+    "https://discord.com/api/webhooks/771035146115481630/DY6_A9MS0lgNsBMrMkauynW9OaUYIwofSqVA4SSVB2Ly9IBlbeNlxyIc7-CmFrz-fpIs"
+  );
+
+  const msg = new webhook.MessageBuilder()
+    .setName("🍯 Honeypot")
+    .setColor("#00fa9a")
+    .setAuthor("🍯 Honeypot", null, "https://github.com/aboutdavid/honeypot")
+    .setText("This is my webhook!")
+    .addField("IP address:", ip, false)
+    .addField("Trap URL:", path, false)
+    .addField("Description of trap:", desc, false)
+    .setFooter(`Attacked recorded on: ${date}`, null);
+  Hook.send(msg);
 }
-module.exports = {send}
-send();
+module.exports = { send };
+send(new Date());
